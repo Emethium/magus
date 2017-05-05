8de9cda66a4fba2fdb68d788e5a7e54aa062b330
emethium <nikolas.serafini@gmail.com>
2017-05-05 10:06:42 -0300

    Translated mockup views to portuguese

--- app/app/helpers/application_helper.rb
+++ app/app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # Returns the full title on a per-page basis.
   def full_title(page_title = '')
-    base_title = "Mailman, the SMS mailer"
+    base_title = "DoaçãoSMS"
     if page_title.empty?
       base_title
     else
