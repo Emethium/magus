a1311741f100a5b7f2798088506e71e83bf60d07
emethium <nikolas.serafini@gmail.com>
2017-05-05 14:19:39 -0300

    Donator views done, but with a misterious form object rollback

--- app/app/controllers/donators_controller.rb
+++ app/app/controllers/donators_controller.rb
@@ -1,4 +1,31 @@
 class DonatorsController < ApplicationController
   def new
+    @donator = Donator.new
   end
+
+  def create
+    @donator = Donator.new(donator_params)
+    if @donator.save
+      flash[:success] = "O doador foi cadastrado com sucesso!"
+      redirect_to @donator
+    else
+      flash[:success] = "Ehr, alguma coisa deu errado!"
+      render 'new'
+    end
+  end
+
+  def show
+    @donator = Donator.find(params[:id])
+  end
+
+  def index
+    @donators = Donator.all
+  end
+
+  private
+    def donator_params
+      params.require(:donator).permit(:name, :sex, :address, :phone, :bloodType,
+                                      :rhFactor, :age)
+    end
+
 end
