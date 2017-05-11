0795aa441a1677743d062eb7248fca76a64f774d
emethium <nikolas.serafini@gmail.com>
2017-05-11 09:32:26 -0300

    Commiting last updates for traveling coding

--- app/app/controllers/donators_controller.rb
+++ app/app/controllers/donators_controller.rb
@@ -9,7 +9,7 @@ class DonatorsController < ApplicationController
       flash[:success] = "O doador foi cadastrado com sucesso!"
       redirect_to @donator
     else
-      flash[:success] = "Ehr, alguma coisa deu errado!"
+      flash[:error] = "Ehr, alguma coisa deu errado!"
       puts @donator.errors.full_messages
       render 'new'
     end
@@ -23,10 +23,26 @@ class DonatorsController < ApplicationController
     @donators = Donator.all
   end
 
+  def edit
+    @donator = Donator.find(params[:id])
+    if @donator.update_attributes(full_donator_params)
+      flash[:success] = "Cadastro atualizado com sucesso!"
+      redirect_to @donator
+    else
+      flash[:error] = "Erh, algum erro ocorreu. Tente novamente!"
+      render 'edit'
+    end
+  end
+
   private
     def donator_params
       params.require(:donator).permit(:name, :sex, :address, :phone, :bloodType,
                                       :rhFactor, :age)
+    end
+
+    def full_donator_params
+      params.permit(:name, :sex, :address, :phone, :bloodType,
+                                      :rhFactor, :age, :lastDonation)
     end
 
 end
