3a593aa5b375be4ece7e0906ce010767d16618ef
emethium <nikolas.serafini@gmail.com>
2017-06-09 14:49:51 -0300

    Done SMS model views

---
+++ app/app/controllers/sms_models_controller.rb
@@ -0,0 +1,38 @@
+class SmsModelsController < ApplicationController
+  def new
+    @sms_model = SmsModel.new
+  end
+
+  def create
+    @sms_model = SmsModel.new(sms_model_params)
+    if sms_model.save
+      flash[:success] = "O seu novo modelo foi cadastrado com sucesso!"
+      redirect_to models_path
+    else
+      flash[:error] = "Ehr, alguma coisa deu errado aqui. Tente novamente!"
+      redirect_to newmodels_path
+    end
+  end
+
+  def show
+    @sms_model = SmsModel.find(params[:id])
+  end
+
+  def index
+    @sms_models = SmsModel.all
+  end
+
+  def edit
+    @sms_model = SmsModel.find(params[:id])
+  end
+
+  def update
+
+  end
+
+  private
+    def sms__model_params
+      params.require(:sms_model).permit(:name, :content)
+    end
+
+end
